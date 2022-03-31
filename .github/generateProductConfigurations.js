2d78e81567b5d73cf8c80915bf960e63374e5eae
Liangcheng Juves <liangchengj@outlook.com>
2022-03-31 19:25:40 +0800

    Update `server-web` packages' manifest configuration

--- .github/generateProductConfigurations.js
+++ .github/generateProductConfigurations.js
@@ -37,7 +37,8 @@ function generateProductJson() {
   product["win32UserAppId"] = vsProduct["win32UserAppId"];
   product["win32x64UserAppId"] = vsProduct["win32x64UserAppId"];
   product["win32arm64UserAppId"] = vsProduct["win32arm64UserAppId"];
-  product["win32ShellNameShort"] = _replaceName(product["win32ShellNameShort"]).replace(" ", "");
+  product["win32ShellNameShort"] = _replaceName(product["win32ShellNameShort"])
+    .replace(" ", "");
   product["darwinBundleIdentifier"] = vsProduct["darwinBundleIdentifier"];
   product["darwinExecutable"] = "CodeOSS";
   product["dataFolderName"] = `.${product["applicationName"]}`;
@@ -124,5 +125,29 @@ function generateWin32VisualElementsManifest() {
   console.log(`Generate "${outputFileName}" successfully!`);
 }
 
+function generateServerWebManifest() {
+  const outputFileName = "manifest.json";
+  const product = require("../product.json");
+
+  const manifestJsonPath = path.join(
+    process.cwd(),
+    "resources",
+    "server",
+    outputFileName,
+  );
+
+  const manifest = JSON.parse(fs.readFileSync(manifestJsonPath).toString());
+  manifest["name"] = product["nameLong"];
+  manifest["short_name"] = product["nameShort"];
+
+  fs.writeFileSync(
+    manifestJsonPath,
+    JSON.stringify(manifest),
+  );
+
+  console.log(`Generate "${outputFileName}" successfully!`);
+}
+
 generateProductJson();
 generateWin32VisualElementsManifest();
+generateServerWebManifest();
